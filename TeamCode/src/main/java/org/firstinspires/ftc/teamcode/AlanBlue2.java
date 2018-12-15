dae509025bf60f82786f9bbf79263c6d55b9a01f
Kian Elliott <kianelliott@hotmail.com>
2018-12-15 11:31:39 -0500

    Created Alan Blue 2 Autonimous mode (facing blue depot from lander).  Consists of landing and sampling code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlanBlue2.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Alan Blue 2",group = "Autonomous")
+public class AlanBlue2 extends AlanAutonomousHardwareMap {
+    // BoxyHardwareMap robot = new BoxyHardwareMap();
+    private ElapsedTime runtime = new ElapsedTime();
+    GoldAlignDetector align;
+    public final int LIFT_RUN_POSITION = -2310;
+    public final int LIFT_DOWN_POSITION = -1000;
+    // BoxyHardwareMap robot = new BoxyHardwareMap();
+    // private ElapsedTime runtime = new ElapsedTime();
+    //GoldAlignDetector align;
+
+    public void runOpMode() throws InterruptedException {
+        // STATE 0
+        //initialize the motors and variables
+        // This MUST be here or else there are errors in loading the motors
+        // which end up causing encoderDrive to loop.
+        init(hardwareMap);
+        align = new GoldAlignDetector();
+        align.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        align.alignSize = 180;
+        int state = 10;
+        //wait till start here in the this place
+        align.enable();
+        waitForStart();
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if(state == 10) {
+            telemetry.addData("State","10");
+            telemetry.update();
+            setLiftPosition(LIFT_RUN_POSITION, 0.3);
+            hookServo.setPosition(1);
+            sleep(100);
+            setLiftPosition(LIFT_DOWN_POSITION, 0.3);
+            hookServo.setPosition(0.15);
+            state = 20;
+        }
+
+        //State 30
+        if (state == 20) {
+            sleep(500);
+            //if the gold IS on screen
+            //if (align.isFound()) {
+            if ((align.getXPosition() < align.xMax() && align.getXPosition() > align.xMin())||(align.getAligned())) {
+                // if already aligned (in center), just go straight
+                state = 23;
+                telemetry.addData("Loading State", "23");
+            } else if (align.getXPosition() < align.xMin()) {
+                // if the gold is to the left of the window of "aligned";
+                // if the robot needs to turn left to be aligned with the gold
+                state = 21;
+                telemetry.addData("Loading State", "21");
+            } else {
+                // if the gold is on the right (blindspot), turn right
+                state = 22;
+                telemetry.addData("Loading State", "22");
+            }
+            telemetry.update();
+            sleep(200);
+            align.disable();
+        }
+
+        // STATE 31
+        if (state == 22) {
+            telemetry.addData("State","31");
+            telemetry.addData("Status","Turning Right");
+            telemetry.update();
+            navxTurnRel(-46);
+            //while(!align.getAligned() && opModeIsActive()) {
+            // navxTurnRel(-2,1);
+            //sleep(50);
+            //}
+            // Relative
+            sleep(500);
+            // CHANGE THESE VALUES
+            telemetry.addData("Status","Moving");
+            telemetry.update();
+            encoderDrive(0.2, -26, -26, -26, -26, 3);
+            telemetry.addData("Status","Turning Left");
+            telemetry.update();
+            navxTurnRel(53);
+            // CHANGE THESE VALUES
+            telemetry.addData("Status","Moving");
+            telemetry.update();
+            encoderDrive(0.2, -22, -22, -22, -22, 3);
+            stopMotors();
+            telemetry.addLine("Done");
+            telemetry.update();
+        }
+
+
+        // STATE 32
+        if (state == 21) {
+            telemetry.addData("State","21");
+            telemetry.addData("Status","Turning Left");
+            telemetry.update();
+            // Relative
+            navxTurnRel(46);
+            telemetry.addData("Status","Moving");
+            telemetry.update();
+            // CHANGE THESE VALUES
+            encoderDrive(0.2, -26, -26, -26, -26, 3);
+            telemetry.addData("Status","Turning Right");
+            telemetry.update();
+            navxTurnRel(-53);
+            // CHANGE THESE VALUES
+            telemetry.addData("Status","Moving");
+            telemetry.update();
+            encoderDrive(0.2, -7,  -7, -7, -7, 3);
+            telemetry.addData("Status","Turning Left");
+            telemetry.update();
+            navxTurnRel(90);
+          //  state = 30;
+        }
+
+        // STATE 33
+        if (state == 23) {
+            // CHANGE THESE VALUES
+            telemetry.addData("Status","Moving");
+            telemetry.update();
+            encoderDrive(0.2, -35, -35, -35, -35, 2);
+            stopMotors();
+        }
+
+        align.disable();
+
+    }
+}
