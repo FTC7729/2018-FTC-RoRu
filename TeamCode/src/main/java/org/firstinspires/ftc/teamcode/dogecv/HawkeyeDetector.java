7021a741e249920d3be03f89851b77f93df3a4eb
Isaac Blum <ibtronfan@gmail.com>
2019-01-08 20:26:49 -0500

    Replace blue filter with green filter on the Hawkeye detection class. Needs debugging as the colors are proving inconsistent.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dogecv/HawkeyeDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dogecv/HawkeyeDetector.java
@@ -1,16 +1,19 @@
 package org.firstinspires.ftc.teamcode.dogecv;
 
 import com.disnodeteam.dogecv.detectors.GenericDetector;
+import com.disnodeteam.dogecv.filters.HSVColorFilter;
 import com.disnodeteam.dogecv.filters.LeviColorFilter;
 import com.disnodeteam.dogecv.scoring.MaxAreaScorer;
 import com.disnodeteam.dogecv.scoring.PerfectAreaScorer;
 import com.disnodeteam.dogecv.scoring.RatioScorer;
 
+import org.opencv.core.Scalar;
+
 public class HawkeyeDetector extends GenericDetector {
     public HawkeyeDetector() {
         super();
         detectorName = "Hawkeye Collision Detector";
-        colorFilter = new LeviColorFilter(LeviColorFilter.ColorPreset.BLUE);
+        colorFilter = new HSVColorFilter(new Scalar(85,225,225), new Scalar(45,25,25));
         perfectAreaScorer = new PerfectAreaScorer(40000,4);
         maxDifference = 6;
     }
